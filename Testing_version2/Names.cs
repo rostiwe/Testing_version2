21869b1e5792a99b3d67d86f430ae7dc8fd332ef
rostiwe <rostisla.lysenkov@mail.ru>
2020-02-16 02:04:41 +0300

    2

---
+++ Testing_version2/Names.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing_version2
+{
+    static class Names
+    {
+        public static void Shake ()
+        {
+            Console.Write("Введите имя: ");
+            string Name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string SurName = Console.ReadLine(), resoult = "";
+            for (int i = 0; i < Name.Length; i++)
+            {
+                resoult += Convert.ToChar(Name[i] ^ SurName[i]);        
+                Console.WriteLine("Буквы: " + Name[i] + " и " + SurName[i] + ". Их коды:" + Convert.ToInt32(Name[i]) + " и " + Convert.ToInt32(SurName[i]) + ". После операции XOR: " + (Name[i] ^ SurName[i]));
+                if (SurName.Length == (i + 1) && (i + 1) != Name.Length)
+                {
+                    resoult += Name.Substring((i + 1), (Name.Length - (i + 1)));
+                    break;
+                }
+                if (Name.Length == (i + 1) && (i + 1) != SurName.Length)
+                    resoult += SurName.Substring((i + 1), (SurName.Length - (i + 1)));//
+            }
+            Console.WriteLine("Результат: \n" + resoult);
+        }
+    }
+}
--- Testing_version2/Program.cs
+++ Testing_version2/Program.cs
@@ -12,18 +12,25 @@ namespace Testing_version2
             string SurName = Console.ReadLine(), resoult = "";
             for (int i = 0; i < Name.Length; i++)
             {
-                char Name_char = Name[i], SurName_char = SurName[i];
-                resoult += Convert.ToChar(Name_char ^ SurName_char);
-                Console.WriteLine("Буквы: " + Name[i] + " и " + SurName[i] + ". Их коды:" + Convert.ToInt32(Name_char) + " и " + Convert.ToInt32(SurName_char)+". После операции XOR: "+(Name_char ^ SurName_char));
-                if(SurName.Length == (i + 1) && (i+1) != Name.Length)
+                resoult += Convert.ToChar(Name[i] ^ SurName[i]);
+                // Name[i] - символ(char), из строки Name. К двум строкам нельзя применять побитовые операции, а к 2 символам можно.
+                Console.WriteLine("Буквы: " + Name[i] + " и " + SurName[i] + ". Их коды:" + Convert.ToInt32(Name[i]) + " и " + Convert.ToInt32(SurName[i]) +". После операции XOR: "+(Name[i] ^ SurName[i]));
+                if(SurName.Length == (i + 1) && (i + 1) != Name.Length)
+                // если мы подошли к концу SurName, но name ещё не закончилось.(SurName.Length - возвращает общее количество символов, а в SurName[i] счёт начинается с 0, отсюда +1)
                 {
                     resoult += Name.Substring((i + 1), (Name.Length - (i + 1)));
-                    break;
+                // Добавим к результату часть строки Name, которую не трогали. Name.Substring(a,b) возвращает часть строки Name, начиная с символа с номером a. Всего возвращает  b - символов.
+                    break; // заканчиваем цикл for, т.к. достигли конца одной из строк
                 }
                 if(Name.Length == (i + 1) && (i + 1) != SurName.Length)
-                    resoult += SurName.Substring((i + 1), (SurName.Length - (i + 1)));
+                // Аналогично с кодом выше, но если заканчивается Name, а в SurName ещё есть символы. for не прерываем - он и так сейчас закончится.
+                    resoult += SurName.Substring((i + 1), (SurName.Length - (i + 1)));//
             }
-            Console.WriteLine(resoult);
+            Console.WriteLine( "Результат: \n"+resoult);
+            //Конец
+            // Вариант 2 (как стоит делать). Что бы не засорять main просто перенесём код в статичеcкий метод класса Names  
+            // Используем этот метод
+            Names.Shake();
         }
     }
 }
